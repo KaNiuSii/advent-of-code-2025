e7c9b2256651a43288bef1841e455f0123abc260
Kaniusii <kacper.woznica002@gmail.com>
2025-12-25 02:21:18 +0100

    asd

--- 6/6.py
+++ 6/6.py
@@ -27,18 +27,13 @@ def part1():
     print(f'Part 1: {ans}')
 
 
-def fill_whitespace(grid: List[List[str]]):
+def fill_whitespace(grid: List[str]):
     copy = grid.copy()
-    for row in range(len(copy)):
-        sequence = copy[row]
-        longest = max([len(x) for x in sequence])
-        for col in range(len(copy[row])):
-            element = copy[row][col]
-            if element == sequence[-1]:
-                continue
-            prefix = (' ' * (longest - len(element)))
-            filled = ''.join([prefix, element])
-            copy[row][col] = filled
+
+    max_len = max([len(x) for x in copy])
+
+    for i in range(len(copy)):
+        copy[i] += ' ' * (max_len - len(copy[i]))
 
     return copy
 
@@ -46,6 +41,7 @@ def part2():
     ans = 0
 
     input_lines = get_input().split('\n')
+    input_lines = fill_whitespace(input_lines)
     last_line = input_lines[-1]
 
     idx_operators = [x for x in range(len(last_line)) if last_line[x] != ' ']
@@ -61,23 +57,31 @@ def part2():
                 part = line[idx:next_idx]
             else:
                 part = line[idx:]
+
             row.append(part)
+
+        if all([e[-1] for e in row]):
+            row = [e[:-1] for e in row]
+
         grid.append(row)
+
+    grid.append(last_line.split())
+
+    grid = rewrite(grid)
+
     print(grid)
-    # grid = rewrite(grid)
-    # grid = fill_whitespace(grid)
-    #
-    # num_count = len(grid[0][0])
-    #
-    # for row in grid:
-    #     old_nums, operation = row[:-1], row[::-1][0]
-    #     new_nums = []
-    #     for num_idx in range(num_count):
-    #         new_nums.append(''.join([x[num_idx] for x in old_nums if x[num_idx] != ' ']))
-    #     new_nums = new_nums[::-1]
-    #     evaluation = ''.join([new_nums[0]] + [operation + new_nums[x] for x in range(len(new_nums)) if x != 0])
-    #     ans += int(eval(evaluation))
-    #     print(f'{old_nums} {evaluation} = {int(eval(evaluation))}')
+
+    num_count = len(grid[0][0])
+
+    for row in grid:
+        old_nums, operation = row[:-1], row[::-1][0]
+        new_nums = []
+        for num_idx in range(num_count):
+            new_nums.append(''.join([x[num_idx] for x in old_nums if x[num_idx] != ' ']))
+        new_nums = new_nums[::-1]
+        evaluation = ''.join([new_nums[0]] + [operation + new_nums[x] for x in range(len(new_nums)) if x != 0])
+        ans += int(eval(evaluation))
+        print(f'{old_nums} {evaluation} = {int(eval(evaluation))}')
 
     print(f'Part 2: {ans}')
 
