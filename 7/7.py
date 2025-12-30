6b0dc6d836016c3dedd32888f15a142c2cfadbfd
Kacper Woźnica <kacper.woznica@r.pl>
2025-12-30 21:13:05 +0100

    7 init

---
+++ 7/7.py
@@ -0,0 +1,54 @@
+
+def get_input():
+    with open("7test.txt", "r") as f:
+        return f.read().split()
+
+
+def part1():
+    ans = 0
+
+    puzzle = get_input()
+    puzzle_without_empty = [puzzle[i] for i in range(len(puzzle)) if i % 2 == 0]
+
+    objects_idx = [puzzle_without_empty[0].index('S')]
+
+    for splitter_line in puzzle_without_empty[1:]:
+        splitters_idx = [i for i in range(len(splitter_line)) if splitter_line[i] == '^']
+        duplicates = set(objects_idx) & set(splitters_idx)
+        for duplicate in duplicates:
+            left, right = duplicate - 1, duplicate + 1
+            objects_idx.remove(duplicate)
+            if left not in objects_idx:
+                objects_idx.append(left)
+            if right not in objects_idx:
+                objects_idx.append(right)
+            ans += 1
+
+    print(f'Part1: {ans}')
+
+def part2():
+    ans = 1
+
+    puzzle = get_input()
+    puzzle_without_empty = [puzzle[i] for i in range(len(puzzle)) if i % 2 == 0]
+
+    objects_tree = []
+
+    objects_idx = [puzzle_without_empty[0].index('S')]
+
+    for splitter_line in puzzle_without_empty[1:]:
+        splitters_idx = [i for i in range(len(splitter_line)) if splitter_line[i] == '^']
+        duplicates = set(objects_idx) & set(splitters_idx)
+        for duplicate in duplicates:
+            left, right = duplicate - 1, duplicate + 1
+            objects_idx.remove(duplicate)
+            if left not in objects_idx:
+                objects_idx.append(left)
+            if right not in objects_idx:
+                objects_idx.append(right)
+
+    print(f'Part2: {ans}')
+
+
+part1()
+part2()
