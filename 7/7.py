425673de53cc7e439d1388ee75317c780a7ee6df
Kacper Woźnica <kacper.woznica@r.pl>
2025-12-31 16:15:37 +0100

    7 init

--- 7/7.py
+++ 7/7.py
@@ -1,3 +1,5 @@
+from idlelib.tree import TreeNode
+from tree import SigmaTree, TreeNode
 
 def get_input():
     with open("7test.txt", "r") as f:
@@ -31,12 +33,15 @@ def part2():
 
     puzzle = get_input()
     puzzle_without_empty = [puzzle[i] for i in range(len(puzzle)) if i % 2 == 0]
+    puzzle_without_first = puzzle_without_empty[1:]
 
-    objects_tree = []
+    first_objects_idx = puzzle_without_empty[0].index('S')
+    objects_idx = [first_objects_idx]
 
-    objects_idx = [puzzle_without_empty[0].index('S')]
+    tree = SigmaTree(first_objects_idx)
 
-    for splitter_line in puzzle_without_empty[1:]:
+    for splitter_line_id in range(len(puzzle_without_first)):
+        splitter_line = puzzle_without_first[splitter_line_id]
         splitters_idx = [i for i in range(len(splitter_line)) if splitter_line[i] == '^']
         duplicates = set(objects_idx) & set(splitters_idx)
         for duplicate in duplicates:
@@ -46,6 +51,11 @@ def part2():
                 objects_idx.append(left)
             if right not in objects_idx:
                 objects_idx.append(right)
+        # print(objects_idx)
+        tree.add_range([TreeNode(x) for x in objects_idx])
+
+    tree.print_tree()
+    ans = tree.get_paths()
 
     print(f'Part2: {ans}')
 
