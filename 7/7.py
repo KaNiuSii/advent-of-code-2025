08389319611f53a3ef65407dadc74cf6540ebb9b
Kaniusii <kacper.woznica002@gmail.com>
2026-01-02 02:41:26 +0100

    i hate day 7, i hate python, i hate my life (day 7 done)

--- 7/7.py
+++ 7/7.py
@@ -1,76 +1,75 @@
+from typing import List
+
 
 def get_input():
-    with open("7test.txt", "r") as f:
+    with open("7.txt", "r") as f:
         return f.read().split()
 
 
 def part1():
     ans = 0
 
+    draw_lines = True
+
     puzzle = get_input()
     puzzle_without_empty = [puzzle[i] for i in range(len(puzzle)) if i % 2 == 0]
 
-    objects_idx = [puzzle_without_empty[0].index('S')]
+    start_id = puzzle_without_empty[0].index('S')
+    particles_idx = [start_id]
+
+    if draw_lines:
+        puzzle[1] = puzzle[1][: start_id] + '|' + puzzle[1][start_id + 1:]
 
     for splitter_line in puzzle_without_empty[1:]:
         splitters_idx = [i for i in range(len(splitter_line)) if splitter_line[i] == '^']
-        hits_in_splitters = set(objects_idx) & set(splitters_idx)
+        hits_in_splitters = set(particles_idx) & set(splitters_idx)
+
         ans += len(hits_in_splitters)
+
         for hit in hits_in_splitters:
             left, right = hit - 1, hit + 1
-            objects_idx.remove(hit)
-            if left not in objects_idx:
-                objects_idx.append(left)
-            if right not in objects_idx:
-                objects_idx.append(right)
+            particles_idx.remove(hit)
+            if left not in particles_idx:
+                particles_idx.append(left)
+            if right not in particles_idx:
+                particles_idx.append(right)
+
+        if draw_lines:
+            i = puzzle.index(splitter_line)
+            for obj in particles_idx:
+                puzzle[i + 1] = puzzle[i + 1][: obj] + '|' + puzzle[i + 1][obj + 1:]
+                puzzle[i] = puzzle[i][: obj] + '|' + puzzle[i][obj + 1:]
+
+    if draw_lines:
+        for line in puzzle:
+            print(line)
 
     print(f'Part1: {ans}')
 
-class ParticleNode:
-    def __init__(self, y: int, x: int, parents: list | None):
-        self.y = y
-        self.x = x
-        self.parents = parents
-        self.left = None
-        self.right = None
-        self.is_divided = False
-
-    def add_parent(self, node):
-        self.parents.append(node)
-
-
-def get_node(nodes: list, y: int, x: int) -> ParticleNode | None:
-    node_arr = [node for node in nodes if node.y == y and node.x == x]
-    if len(node_arr) == 0:
-        return None
-    return node_arr[0]
 
 def part2():
     ans = 0
 
     puzzle = get_input()
     puzzle_without_empty = [puzzle[i] for i in range(len(puzzle)) if i % 2 == 0]
-    y = 0
 
-    first_obj = puzzle_without_empty[0].index('S')
-
-    objects_idx = [first_obj]
-    nodes = [ParticleNode(y, first_obj, None)]
+    start_id = puzzle_without_empty[0].index('S')
+    particles = {start_id: 1}
 
     for splitter_line in puzzle_without_empty[1:]:
-        y += 1
-
         splitters_idx = [i for i in range(len(splitter_line)) if splitter_line[i] == '^']
-        hits_in_splitters = set(objects_idx) & set(splitters_idx)
+        tmp = {}
+        for k, v in particles.items():
+            if k in splitters_idx:
+                left, right = k - 1, k + 1
+                tmp[k] = -v if k not in tmp.keys() else tmp[k] - v
+                tmp[left] = v if left not in tmp.keys() else tmp[left] + v
+                tmp[right] = v if right not in tmp.keys() else tmp[right] + v
 
-        for hit in hits_in_splitters:
-            left, right = hit - 1, hit + 1
-            objects_idx.remove(hit)
-            if left not in objects_idx:
-                objects_idx.append(left)
-            if right not in objects_idx:
-                objects_idx.append(right)
+        for k, v in tmp.items():
+            particles[k] = v if k not in particles.keys() else particles[k] + v
 
+    ans += sum([v for v in particles.values()])
 
     print(f'Part2: {ans}')
 
